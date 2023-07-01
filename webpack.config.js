b33d6bfd62a9a04ca131dbf549b8e34eeaae27a2
hyper <patrykmleczek.pm@gmail.com>
2023-07-01 23:19:22 +0200

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,63 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
     entry: "./src/index.tsx",
     devServer: {
-        contentBase: path.resolve(__dirname, "dist"),
-        publicPath: "/",
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
         port: 3000,
-        hot: true,
-        open: true,
     },
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
                 test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
+                type: "asset/resource",
             },
         ],
     },
-    plugins: [],
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "./public/index.html",
+            // favicon: "",
+            // minify: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+        }),
+    ],
     resolve: {
         extensions: [".tsx", ".ts", ".js"],
     },
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
+        filename: '[name].[chunkhash].js',
         publicPath: "/",
         clean: true,
     },
