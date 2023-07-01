029784b2eb624c2ad0e7d6cc29fd99bf608d5b9c
hyper <patrykmleczek.pm@gmail.com>
2023-07-01 21:52:14 +0200

    Add App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div style={{ height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
